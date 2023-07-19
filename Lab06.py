31b277ae2eaa2bf7070870ac46351dcb1f864913
agiraldo1 <139582278+mattjuranek@users.noreply.github.com>
2023-07-18 21:39:47 -0400

    Added decoded function

--- Lab06.py
+++ Lab06.py
@@ -17,6 +17,25 @@ def encode(inputted_password):
             encoded_password += str(char)
     return encoded_password
 
+
+def decode(encoded_password):
+    decoded_password = ''
+
+    for digit in encoded_password:
+        if digit == '2':
+            decoded_digit = '9'
+        elif digit == '1':
+            decoded_digit = '8'
+        elif digit == '0':
+            decoded_digit = '7'
+        else:
+            decoded_digit = int(digit) - 3
+
+        decoded_password += str(decoded_digit)
+
+    return decoded_password
+
+
 def main():
     program_on = True
     while program_on == True:
@@ -24,9 +43,15 @@ def main():
         option = input('Please enter an option:')
         if option == '1':
             inputted_password = input('Please enter your password to encode:')
+            encoded_password = encode(inputted_password)
             print('Your password has been encoded and stored!')
             pass
         if option == '2':
-            pass
+            decoded_password = decode(encoded_password)
+            print(f"The encoded password is {encoded_password}, and the original password is {decoded_password}.")
         if option == '3':
             program_on = False
+
+
+while __name__ == "__main__":
+    main()
